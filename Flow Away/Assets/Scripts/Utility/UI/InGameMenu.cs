43ab4a3b2cda7d10eacbf32dc160c431d28325af
grravve <55639256+grravve@users.noreply.github.com>
2021-03-01 21:12:54 +0300

    Release InGameMenu

---
+++ Flow Away/Assets/Scripts/Utility/UI/InGameMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    private bool isPaused = false;
+    public GameObject inGameMenu;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            switch(isPaused)
+            {
+                case false:
+                    {
+                        Time.timeScale = 0;
+                        isPaused = true;
+                        inGameMenu.SetActive(true);
+                        break;
+                    }
+                case true:
+                    {
+                        Time.timeScale = 1;
+                        isPaused = false;
+                        inGameMenu.SetActive(false);
+                        break;
+                    }
+            }
+        }
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        isPaused = false; 
+        inGameMenu.SetActive(false);
+    }
+
+    public void Options()
+    {
+        //In progress...
+    }
+
+    public void Menu()
+    {
+        //saving
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
