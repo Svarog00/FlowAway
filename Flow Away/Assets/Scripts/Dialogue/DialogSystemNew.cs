f378272900ea38a5479c895a345ad0a7c4e0d850
Дмитрий Тетерев <diman.teterev@mail.ru>
2021-03-13 09:38:55 +0300

    DialogeInterface done

--- Flow Away/Assets/Scripts/Dialogue/DialogSystemNew.cs
+++ Flow Away/Assets/Scripts/Dialogue/DialogSystemNew.cs
@@ -10,6 +10,7 @@ public class DialogSystemNew : MonoBehaviour
     public List<Answer> answersList = new List<Answer>();
     public bool showDialog;
     public GUISkin GUISkin;
+    public Texture2D npcIconImage;
 
     private int curNode;
 
@@ -45,11 +46,12 @@ public class DialogSystemNew : MonoBehaviour
         if(showDialog)
         {
             Time.timeScale = 0;
-            GUI.Box(new Rect(Screen.width / 2 - 300, Screen.height - 300, 600, 300), ""); //Создание бокса с ответами
-            GUI.Label(new Rect(Screen.width / 2 - 145, Screen.height - 242, 400, 90), dialogue.nodes[curNode].npcText);//Фраза НПС
+            GUI.Box(new Rect(Screen.width / 2 - 375, Screen.height - 408, 750, 400), ""); //Создание бокса с ответами
+            GUI.DrawTexture(new Rect(Screen.width / 2 - 300, Screen.height - 340, 128, 128), npcIconImage);
+            GUI.Label(new Rect(Screen.width / 2 - 160, Screen.height - 335, 450, 110 ), dialogue.nodes[curNode].npcText);//Фраза НПС 
             for (int i = 0; i < answersList.Count; i++)
             {
-                if (GUI.Button(new Rect(Screen.width / 2 - 250, Screen.height - 150 + 28 * i, 500, 25), answersList[i].text))//Если нажата кнопка с текстом ответа
+                if (GUI.Button(new Rect(Screen.width / 2 - 250, Screen.height - 215 + 28 * i, 500, 25), answersList[i].text))//Если нажата кнопка с текстом ответа
                 {
                     if(answersList[i].questValue > 0)
                     {
