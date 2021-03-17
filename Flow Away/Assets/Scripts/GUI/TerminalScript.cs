acbb5bfa6bcc29c84ffde42ba126e65858b66ecd
Дмитрий Тетерев <diman.teterev@mail.ru>
2021-03-17 18:35:10 +0300

    Terminal prototype added

---
+++ Flow Away/Assets/Scripts/GUI/TerminalScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalScript : MonoBehaviour
+{
+    public GUISkin GUISkin;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnGUI() 
+    {
+        GUI.skin = GUISkin;
+        Time.timeScale = 0;
+        GUI.Box(new Rect(Screen.width / 2 - 700, Screen.height - 800, 1400, 700), ""); //Создание бокса с ответами
+    }
+}
