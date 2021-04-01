ef1936db4e321a1cc77f6c6e1cafbfd225840fe5
Svarog00 <54817442+Svarog00@users.noreply.github.com>
2021-04-01 21:49:44 +0300

    Power shield gadget started

---
+++ Flow Away/Assets/Scripts/Player/HandlePowerShield.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HandlePowerShield : MonoBehaviour
+{
+    public event EventHandler<OnShieldActivatedEventArgs> OnShieldActivated;
+    public class OnShieldActivatedEventArgs
+    {
+        public bool isAcive;
+    }
+
+
+    public bool CanActivate { get; set; }
+
+    private bool _isActve;
+
+    private void Start()
+    {
+        CanActivate = QuestValues.Instance.GetStage("PowerShield") > 0 ? true : false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("SecondModule") && CanActivate)
+        {
+            if (!_isActve)
+            {
+                _isActve = true;
+                OnShieldActivated?.Invoke(this, new OnShieldActivatedEventArgs { isAcive = _isActve });
+            }
+            else
+            {
+                _isActve = false;
+                OnShieldActivated?.Invoke(this, new OnShieldActivatedEventArgs { isAcive = _isActve });
+            }
+        }
+    }
+}
---
+++ Flow Away/Assets/Scripts/Player/PowerShield.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerShield : MonoBehaviour, IDamagable
+{
+    public float time;
+
+    [SerializeField] private int _capacity;
+    [SerializeField] private float _maxTime;
+    private bool _isActve;
+    private float _curTime;
+    [SerializeField] private GameObject _shield;
+
+    private void Start()
+    {
+        HandlePowerShield handlePowerShield = GetComponentInParent<HandlePowerShield>();
+        handlePowerShield.OnShieldActivated += HandlePowerShield_OnShieldActivated;
+        _isActve = false;
+    }
+
+    private void HandlePowerShield_OnShieldActivated(object sender, HandlePowerShield.OnShieldActivatedEventArgs e)
+    {
+        _isActve = e.isAcive;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void Hurt(int damage)
+    {
+        _capacity -= damage;
+    }
+}
