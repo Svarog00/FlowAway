217daf465d0a0667d3c34c05e7082412a46c9c33
grravve <55639256+grravve@users.noreply.github.com>
2021-04-01 21:42:35 +0300

    Terminal with notes is done

--- Flow Away/Assets/Scripts/GUI/Notes/NotesScript.cs
+++ Flow Away/Assets/Scripts/GUI/Notes/NotesScript.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 using System.Xml.Serialization;
 using System.IO;
 
-public class NotesScript : MonoBehaviour
-{
-    
-}
+    [XmlRoot("terminal")]
+    public class Terminal
+    {
+        [XmlArray("notes")]
+        [XmlArrayItem("note")]
+        public Note[] notes;
+
+        public static Terminal Load(TextAsset _xml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Terminal));
+            StringReader reader = new StringReader(_xml.text);
+            Terminal terminal = serializer.Deserialize(reader) as Terminal;
+            return terminal;
+        }
+    }
+
+    [System.Serializable]
+    public class Note
+    {
+        [XmlAttribute("title")]
+        public string title;
+        [XmlElement("text")]
+        public string text;
+    }
+
